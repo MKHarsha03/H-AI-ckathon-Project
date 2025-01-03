ebe2a725a2a8babca79d3eb29cb017338c0955d3
MKHarsha03 <124070180+MKHarsha03@users.noreply.github.com>
2025-01-03 08:04:18 +0530

    Testing sending data from frontend form to backend using flask

--- flask_application/hello.py
+++ flask_application/hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import request
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/submit",methods=['POST'])
+def submit():
+    name=request.form['name']
+    email=request.form['email']
+
+    print(f"Name: {name},Email: {email}")
+    return f"Recieved data:Name={name},Email={email}"
--- my-hackathon-project/src/home.jsx
+++ my-hackathon-project/src/home.jsx
@@ -1,21 +1,14 @@
-import './index.css';
-import Page1 from './page1.jsx';
-import Page2 from './page2.jsx';
-import Page3 from './page3.jsx';
-import {BrowserRouter,Routes,Route,NavLink} from 'react-router-dom';
 export default function Home(){
     return(
-        <BrowserRouter>
-                <nav>
-                    <NavLink to="/page1">Link 1</NavLink>
-                    <NavLink to="/page2">Link 2</NavLink>
-                    <NavLink to="/page3">Link 3</NavLink>
-                </nav>
-                <Routes>
-                    <Route path="/page1" element={<Page1 />}/>
-                    <Route path="/page2" element={<Page2 />}/>
-                    <Route path="/page3" element={<Page3 />}/>
-                </Routes>
-        </BrowserRouter>
+        <>
+            <form action="http://127.0.0.1:5000/submit" method='post'>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name"/><br/><br/>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email"/><br/><br/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </>
+    
     );
 }
